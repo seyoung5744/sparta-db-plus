6ca69358af7ac8131ddc10b4a9d0c6ed52baffb4
seyoung5744 <seyoung574458@gmail.com>
2025-09-10 16:11:00 +0900

    데이터베이스 데이터를 외부에서 다루기 - 3. JDBC template

---
+++ jdbc/src/main/java/com/example/jdbc/DataRepository.java
@@ -0,0 +1,41 @@
+package com.example.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DataRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    // 테이블 생성
+    public void createTable() {
+        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS users (id SERIAL, name VARCHAR(255))");
+    }
+
+    // 사용자 추가 (Create)
+    public void insertUser(String name) {
+        jdbcTemplate.update("INSERT INTO users (name) VALUES (?)", name);
+    }
+
+    // 사용자 ID로 User 조회 (Read)
+    public User findUserById(Long id) {
+        return jdbcTemplate.queryForObject(
+                "SELECT * FROM users WHERE id = ?",
+                new UserRowMapper(), // 이자리에 매퍼를 생성자로 넣어주면 됨
+                id
+        );
+    }
+
+    // 사용자 이름 변경 (Update)
+    public void updateUser(Long id, String newName) {
+        jdbcTemplate.update("UPDATE users SET name = ? WHERE id = ?", newName, id);
+    }
+
+    // 사용자 삭제 (Delete)
+    public void deleteUser(Long id) {
+        jdbcTemplate.update("DELETE FROM users WHERE id = ?", id);
+    }
+}
---
+++ jdbc/src/main/java/com/example/jdbc/UserRowMapper.java
@@ -0,0 +1,18 @@
+package com.example.jdbc;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserRowMapper implements RowMapper<User> {
+
+    // JDBCTemplate 에서 row 응답을 mapRow() 메서드에 rs 파라미터로 넘겨주어 객체에 매핑하기 쉽도록 도와준다.
+    @Override
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+        var user = new User();
+        user.setId(rs.getInt("ID"));
+        user.setName(rs.getString("NAME"));
+        return user;
+    }
+}
